44bcb912d56f5a1977cc442e0b67c2f205a41d89
Tibu EngineF <tibu@enginef.com>
2021-12-01 19:15:47 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # node-red-contrib-aws-s3-adapter
+
+This is a helper node that uses the aws-sdk & bluebird to upload files into S3 bucket.
+
+## Mandatory/Default Inputs - on all nodes
+
+**Name**  
+Display name of the Node.
+
+**TargetAttribute**
+Defines the Attribute of the `msg` Object that will store the location url.  
+Defaults to `msg.payload`.
+
+**Access Key Id**  
+Access Key Id of the user with programmatic access to S3.
+
+**Secret Access Key**  
+Secret Access Key of the user with programmatic access to S3.
+
+**Region**  
+Region of the S3 bucket. eg: eu-west-1
+
+**Bucket Name**  
+Name of the S3 bucket.
+
+## Inputs
+
+**File Name**  
+Here we normally use the userId of an account to store the items for a specific user.  
+This can also be provided as a parameter to the inbound message on attribute `msg.userId`.  
+If the `msg.userId` is present on the input, it will be removed before the message is send forward.
+
+**Base64 Encoded**  
+The Base64 encode string of the file.  
+This can also be provided as a parameter to the inbound message on attribute `msg.base64`.  
+If the `msg.base64` is present on the input, it will be removed before the message is send forward.
+
+**Folder Name**  
+The Folder Name in which the file is to be uploaded.  
+This can also be provided as a parameter to the inbound message on attribute `msg.folderName`.  
+If the `msg.folderName` is present on the input, it will be removed before the message is send forward.
+
+**File Type**  
+The type of the file ie: jpeg, png or gif.  
+This can also be provided as a parameter to the inbount message on attribute `msg.fileType`.  
+If the `msg.fileType` is present on the input, it will be removed before the message is send foreward.
+
+## Output
+
+`msg.payload`
+
+## Dependencies
+
+- aws-sdk
+- bluebird
---
+++ s3-upload.js
@@ -0,0 +1,161 @@
+const AWS = require("aws-sdk");
+
+const uploadToS3 = ({
+  ACCESS_KEY_ID,
+  SECRET_ACCESS_KEY,
+  AWS_REGION,
+  S3_BUCKET,
+  userId,
+  base64,
+  folderName,
+  fileType,
+}) =>
+  new Promise((resolve, reject) => {
+    // Configure AWS to use promise
+    AWS.config.setPromisesDependency(require("bluebird"));
+    AWS.config.update({
+      accessKeyId: ACCESS_KEY_ID,
+      secretAccessKey: SECRET_ACCESS_KEY,
+      region: AWS_REGION,
+    });
+
+    // Create an s3 instance
+    const s3 = new AWS.S3();
+
+    // Ensure that you POST a base64 data to your server.
+    const base64Data = Buffer.from(
+      base64.replace(/^data:image\/\w+;base64,/, ""),
+      "base64"
+    );
+
+    const params = {
+      Bucket: S3_BUCKET,
+      Key: `${folderName}/${userId}.${fileType}`, // type is not required
+      Body: base64Data,
+      ACL: "public-read",
+      ContentEncoding: "base64", // required
+      ContentType: `image/${fileType}`, // required. file type, ie: jpeg, png or gif
+    };
+
+    let location = "";
+    let key = "";
+    try {
+      s3.upload(params)
+        .promise()
+        .then(({ Location, Key }) => {
+          location = Location;
+          key = Key;
+          // Save the Location (url) to the database and Key if needed.
+          // As good developers, we should return the url and let other function do the saving to database etc
+          console.log(location, key);
+
+          resolve(location);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    } catch (error) {
+      console.log(error);
+      reject(error);
+    }
+  });
+
+module.exports = function (RED) {
+  function s3FileUpload(config) {
+    RED.nodes.createNode(this, config);
+
+    var userId = this.file_data?.userId;
+    var base64 = this.file_data?.base64;
+    var folderName = this.file_data?.folderName;
+    var fileType = this.file_data?.fileType;
+    var node = this;
+
+    node.on("input", function (msg) {
+      if (
+        msg.hasOwnProperty("userId") ||
+        msg.hasOwnProperty("base64") ||
+        msg.hasOwnProperty("fileType") ||
+        msg.hasOwnProperty("folderName")
+      ) {
+        userId = msg.userId;
+        delete msg.userId;
+        base64 = msg.base64;
+        delete msg.base64;
+        folderName = msg.folderName;
+        delete msg.folderName;
+        fileType = msg.fileType;
+        delete msg.fileType;
+      }
+
+      const ACCESS_KEY_ID = RED.util.evaluateNodeProperty(
+        config.accessKeyId,
+        config.accessKeyIdType,
+        node,
+        msg
+      );
+      const SECRET_ACCESS_KEY = RED.util.evaluateNodeProperty(
+        config.secretAccessKey,
+        config.secretAccessKeyType,
+        node,
+        msg
+      );
+      const AWS_REGION = RED.util.evaluateNodeProperty(
+        config.awsRegion,
+        config.awsRegionType,
+        node,
+        msg
+      );
+      const S3_BUCKET = RED.util.evaluateNodeProperty(
+        config.s3Bucket,
+        config.s3BucketType,
+        node,
+        msg
+      );
+
+      try {
+        node.status({
+          fill: "yellow",
+          shape: "ring",
+          text: "uploading...",
+        });
+        uploadToS3({
+          ACCESS_KEY_ID,
+          SECRET_ACCESS_KEY,
+          AWS_REGION,
+          S3_BUCKET,
+          userId,
+          base64,
+          folderName,
+          fileType,
+        })
+          .then((response) => {
+            node.status({
+              fill: "green",
+              shape: "ring",
+              text: "upload success",
+            });
+
+            msg.payload = response;
+            node.send([msg, null]);
+          })
+          .catch((error) => {
+            msg.payload = error;
+            node.send([null, msg]);
+            node.status({ fill: "red", shape: "ring", text: error.message });
+          });
+      } catch (error) {
+        msg.payload = error;
+        node.send([null, msg]);
+        this.status({ fill: "red", shape: "ring", text: "error" });
+      }
+    });
+  }
+  RED.nodes.registerType("s3-upload", s3FileUpload, {
+    file_data: {
+      userId: { type: "text" },
+      base64: { type: "text" },
+      folderName: { type: "text" },
+      fileType: { type: "text" },
+    },
+  });
+};
